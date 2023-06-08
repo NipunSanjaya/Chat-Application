1c22e5bd3bbe55ebfd017822dc1873633b3fc18a
chathushkasavinda <chathushkasavinda18@gmail.com>
2023-06-08 14:29:54 +0530

    create controllers

--- src/lk/ijse/chat_application/controller/LoginForm01Controller.java
+++ src/lk/ijse/chat_application/controller/LoginForm01Controller.java
@@ -1,9 +1,24 @@
 package lk.ijse.chat_application.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
 
 public class LoginForm01Controller {
-    public void btnLoginOnAction(ActionEvent actionEvent) {
+    public static String name;
+    public TextField txtName;
+    public AnchorPane loginContext;
 
+    public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
+        name = txtName.getText();
+        loginContext.getChildren().clear();
+        Stage stage = (Stage) loginContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/client-01-form.fxml"))));
     }
 }
